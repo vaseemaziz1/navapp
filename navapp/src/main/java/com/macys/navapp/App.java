9d062d918272b999199c3ab15c9b91c011450e27
Vaseem Aziz <vaseem.aziz@qvantel.com>
2021-08-29 08:55:16 +0530

    Committing code on Duniya branch

--- navapp/src/main/java/com/macys/navapp/App.java
+++ navapp/src/main/java/com/macys/navapp/App.java
@@ -4,12 +4,13 @@ package com.macys.navapp;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello Duniya!" );
         System.out.println( "Hello Aaqirath!" );
+        System.out.println( "Hello Jannah!" );
     }
 }
