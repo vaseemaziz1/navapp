e0813c85a4d0875c8d6a9d15fcdb233b02216f4e
Vaseem Aziz <vaseem.aziz@qvantel.com>
2021-08-29 09:00:33 +0530

    Only duniya

--- navapp/src/main/java/com/macys/navapp/App.java
+++ navapp/src/main/java/com/macys/navapp/App.java
@@ -8,9 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         System.out.println( "Hello Duniya!" );
-        System.out.println( "Hello Aaqirath!" );
-        System.out.println( "Hello Jannah!" );
     }
 }
