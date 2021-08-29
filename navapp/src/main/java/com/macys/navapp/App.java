23c7608a80a07c0e4e3eb5bf29b7baed3a647f9a
Vaseem Aziz <vaseem.aziz@qvantel.com>
2021-08-29 11:41:32 +0530

    Added Jannah

--- navapp/src/main/java/com/macys/navapp/App.java
+++ navapp/src/main/java/com/macys/navapp/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello Duniya!" );
         System.out.println( "Hello Aaqirath!" );
+        System.out.println( "Hello Jannah!" );
     }
 }
